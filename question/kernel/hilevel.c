a0e8576e79a4e51cbbc436155cb2bde931ad316f
M-Mafeni <mm17291@my.bristol.ac.uk>
2019-03-10 20:51:21 +0000

    added P5 process and made it infinite

--- question/kernel/hilevel.c
+++ question/kernel/hilevel.c
@@ -6,7 +6,7 @@
  */
 
 #include "hilevel.h"
-pcb_t pcb[ 2 ]; pcb_t* current = NULL;
+pcb_t pcb[ 3 ]; pcb_t* current = NULL;
 
 void dispatch( ctx_t* ctx, pcb_t* prev, pcb_t* next ) {
   char prev_pid = '?', next_pid = '?';
@@ -40,7 +40,7 @@ void schedule_priority(ctx_t* ctx){
             dispatch(ctx, &pcb[i], &pcb[next]);
             for(int j = 0; j < length; j++){
                 if(j != next){
-                    pcb[next].status = STATUS_READY;
+                    pcb[j].status = STATUS_READY;
                 }
             }
             pcb[next].status = STATUS_EXECUTING;
@@ -63,7 +63,6 @@ void schedule_round_robin( ctx_t* ctx ) {
     pcb[ 1 ].status = STATUS_READY;             // update   execution status  of P_2
     pcb[ 0 ].status = STATUS_EXECUTING;         // update   execution status  of P_1
   }
-
   return;
 }
 
@@ -71,49 +70,58 @@ extern void     main_P3();
 extern uint32_t tos_P3;
 extern void     main_P4();
 extern uint32_t tos_P4;
+extern void     main_P5();
+extern uint32_t tos_P5;
 
 void hilevel_handler_rst( ctx_t* ctx              ) {
-  /* Initialise two PCBs, representing user processes stemming from execution
-   * of two user programs.  Note in each case that
-   *
-   * - the CPSR value of 0x50 means the processor is switched into USR mode,
-   *   with IRQ interrupts enabled, and
-   * - the PC and SP values match the entry point and top of stack.
-   */
+    /* Initialise two PCBs, representing user processes stemming from execution
+    * of two user programs.  Note in each case that
+    *
+    * - the CPSR value of 0x50 means the processor is switched into USR mode,
+    *   with IRQ interrupts enabled, and
+    * - the PC and SP values match the entry point and top of stack.
+    */
 
-   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_1
-   pcb[ 0 ].pid      = 1;
-   pcb[ 0 ].status   = STATUS_CREATED;
-   pcb[ 0 ].ctx.cpsr = 0x50;
-   pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
-   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
+    memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_3
+    pcb[ 0 ].pid      = 1;
+    pcb[ 0 ].status   = STATUS_CREATED;
+    pcb[ 0 ].ctx.cpsr = 0x50;
+    pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
+    pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
 
-   memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_2
-   pcb[ 1 ].pid      = 2;
-   pcb[ 1 ].status   = STATUS_CREATED;
-   pcb[ 1 ].ctx.cpsr = 0x50;
-   pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
-   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
+    memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_4
+    pcb[ 1 ].pid      = 2;
+    pcb[ 1 ].status   = STATUS_CREATED;
+    pcb[ 1 ].ctx.cpsr = 0x50;
+    pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
+    pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
 
- TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
- TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
- TIMER0->Timer1Ctrl |= 0x00000040; // select periodic timer
- TIMER0->Timer1Ctrl |= 0x00000020; // enable          timer interrupt
- TIMER0->Timer1Ctrl |= 0x00000080; // enable          timer
+    memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );     // initialise 2-nd PCB = P_5
+    pcb[ 2 ].pid      = 3;
+    pcb[ 2 ].status   = STATUS_CREATED;
+    pcb[ 2 ].ctx.cpsr = 0x50;
+    pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
+    pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
 
- GICC0->PMR          = 0x000000F0; // unmask all            interrupts
- GICD0->ISENABLER1  |= 0x00000010; // enable timer          interrupt
- GICC0->CTLR         = 0x00000001; // enable GIC interface
- GICD0->CTLR         = 0x00000001; // enable GIC distributor
+    TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
+    TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
+    TIMER0->Timer1Ctrl |= 0x00000040; // select periodic timer
+    TIMER0->Timer1Ctrl |= 0x00000020; // enable          timer interrupt
+    TIMER0->Timer1Ctrl |= 0x00000080; // enable          timer
 
- /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th
-  * PCB to be executed: there is no need to preserve the execution context,
-  * since it is is invalid on reset (i.e., no process will previously have
-  * been executing).
-  */
- dispatch( ctx, NULL, &pcb[ 0 ] );
- int_enable_irq();
-  return;
+    GICC0->PMR          = 0x000000F0; // unmask all            interrupts
+    GICD0->ISENABLER1  |= 0x00000010; // enable timer          interrupt
+    GICC0->CTLR         = 0x00000001; // enable GIC interface
+    GICD0->CTLR         = 0x00000001; // enable GIC distributor
+
+    /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th
+    * PCB to be executed: there is no need to preserve the execution context,
+    * since it is is invalid on reset (i.e., no process will previously have
+    * been executing).
+    */
+    dispatch( ctx, NULL, &pcb[ 0 ] );
+    int_enable_irq();
+    return;
 }
 
 void hilevel_handler_irq(ctx_t* ctx) {
@@ -140,7 +148,7 @@ void hilevel_handler_svc(ctx_t* ctx,uint32_t id) {
             char*  x = ( char* )( ctx->gpr[ 1 ] );
             int    n = ( int   )( ctx->gpr[ 2 ] );
             for( int i = 0; i < n; i++ ) {
-            PL011_putc( UART0, *x++, true );
+                PL011_putc( UART0, *x++, true );
             }
             ctx->gpr[ 0 ] = n;
             break;
